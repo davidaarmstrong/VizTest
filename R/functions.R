6bda6190ec9499d53f197492c07daae95e471618
davidaarmstrong <davearmstrong.ps@gmail.com>
2024-02-22 11:24:36 -0500

    Added plot method and related functions for viztest objects

--- R/functions.R
+++ R/functions.R
@@ -1,3 +1,5 @@
+globalVariables(c("bound_end", "bound_start", "est", "label", "lwr", "stim_end", "stim_start", "upr", "vbl"))
+
 #' Calculate Correspondence Between Pairwise Test and CI Overlaps
 #'
 #' @param obj A model object (or any object) where `coef()` and `vcov()` return estimates of coefficients and sampling variability.
@@ -39,6 +41,7 @@ viztest <- function(obj,
 #' as the threshold in the calculation of "easiness".
 #' @param ... Other arguments, currently not implemented.
 #' @method viztest default
+#' @importFrom dplyr tibble
 #' @export
 viztest.default <- function(obj,
                      test_level = 0.05,
@@ -56,6 +59,7 @@ viztest.default <- function(obj,
     resdf <- obj$df.residual
   }
   bhat <- coef(obj)
+  if(is.null(names(bhat)))names(bhat) <- 1:length(bhat)
   V <- vcov(obj)
   if(!include_intercept){
     w_int <- grep("ntercept", names(bhat))
@@ -94,6 +98,9 @@ viztest.default <- function(obj,
                     psame = apply(s_star, 2, \(x)mean(x == s)),
                     pdiff = mean(s),
                     easy = easiness)
+  est_data <- tibble(vbl = names(bhat), 
+                     est = bhat, 
+                     se = sqrt(diag(V)))
   res <- list(tab = res,
               pw_test = s,
               ci_tests = s_star,
@@ -101,7 +108,8 @@ viztest.default <- function(obj,
               param_names = names(bhat),
               thresh = thresh,
               L = L,
-              U = U)
+              U = U, 
+              est = est_data)
   class(res) <- "viztest"
   return(res)
 }
@@ -242,3 +250,85 @@ vcov.vtcustom <- function(object, ...){
   object$vcov
 }
 
+#' Make Reference Segments
+#' 
+#' Makes reference segments for the `plot.viztest`.  These segments run along the upper bound of a stimulus from the stimulus in question to the last comparison stimulus whose lower bound is smaller than the upper bound of the stimulus in question. 
+#' @param .data Data to be used in the calculating segments.  The data should contain the variables `lwr` and `upr` for the lower and upper confidence bounds, respectively. 
+#' @param vdt Visual difficulty threshold, see the details of `help(plot.viztest)` for an explanation of the parameter. 
+#' @param ... Other arguments to be passed down.  Currently not implemented. 
+#' @returns A data frame with variables `stim_start` and `stim_end` identifying the starting and ending stimuli for the segments as well as `bound_start` and `bound_end` which give the relevant value of the reference bound.  There is also a variable called `ambiguous` which indicates if any comparisons with that bound are ambiguous.
+#' @export
+make_segs <- function(.data, vdt = .02, ...){
+  segs <- NULL
+  for(i in 1:nrow(.data)){
+    if(any(.data$lwr[i:nrow(.data)] < .data$upr[i])){
+      segs <- rbind(segs, data.frame(stim_start=i, stim_end=(i-1) + max(which(.data$lwr[i:nrow(.data)] < .data$upr[i])), bound_start=.data$upr[i], bound_end=.data$upr[i]))
+    }
+  }
+  rg <- max(.data$upr, na.rm=TRUE) - min(.data$lwr, na.rm=TRUE)
+  amb_thresh <- vdt*rg
+  segs$ambiguous <- FALSE
+  for(i in 1:(nrow(segs)-1)){
+    if(any(abs(.data$lwr[(segs$stim_start[i]+1):nrow(.data)] - segs$bound_start[i]) < amb_thresh)){
+      segs$ambiguous[i] <- TRUE
+    }
+  } 
+  segs
+}
+
+
+#' Plot Method for viztest Objects
+#' 
+#' Plots the output of viztest objects with optional reference lines 
+#' @param x Object to be plotted, should be of class `viztest`
+#' @param ref_lines Reference lines to be plotted - one of "all", "ambiguous", "none".  This could also be a vector of stimulus names to plot - they should be the same as the names of the estimates in `x$est`. See details for explanation. 
+#' @param viz_diff_thresh Threshold for identifying visual difficulty, see details. 
+#' @param make_plot Logical indicating whether the plot should be constructed or the data returned. 
+#' @param level Level at which to plot the estimates.  If `NULL` (the default) the easiest optimal level will be chosen.  
+#' @param ... Other arguments passed down.  Currently not implemented.
+#' @details The `ref_lines` argument identifies what reference lines will be plotted in the figure.  For any particular stimulus, the reference lines run along the upper bound of the stimulus from the stimulus location to the most distant stimulus with overlapping confidence intervals.  
+#' When `ref_lines = "all"`, all lines are plotted, though in displays with many stimuli, this can make for a messy graph.  When `"ref_lines = ambiguous"` is specified, then only the ones that help discriminate in cases where the result might be visually difficult to discern are plotted. 
+#' A comparison is determined to be visually difficult if the upper bound of the stimulus in question is within `viz_diff_thresh` times the difference between the smallest lower bound and the largest upper bound.  If `ref_lines = "non"`, then none of the reference lines are plotted. 
+#' Alternatively, you can specify the names of stimuli whose reference lines will be plotted.  These should be the same as the names in the data.  The `viztest()` function returns an object `est`, which contains the data that are used as input to this function.  The variable `vbl` in 
+#' The `est` data frame contains the stimulus names. 
+#' @method plot viztest
+#' @importFrom dplyr left_join arrange `%>%` join_by
+#' @importFrom ggplot2 ggplot geom_pointrange geom_segment aes
+#' @export
+plot.viztest <- function(x, ..., ref_lines="none", viz_diff_thresh = .02, make_plot=TRUE, level=NULL){
+  inp <- x$est
+  if(is.null(level)){
+    tmp <- x$tab[which(x$tab$psame == max(x$tab$psame)), ]
+    tmp <- tmp[which(tmp$easy == max(tmp$easy)), ]
+    level <- tmp$level
+  }
+  w <- which(level == x$tab$level)
+  if(length(w) == 0)stop("level must be one in x$tab$level.\n")
+  inp$lwr <- x$L[,w]
+  inp$upr <- x$U[,w]
+  inp <- inp %>% arrange(est)
+  segs <- make_segs(inp, vdt=viz_diff_thresh)
+  segs$vbl <- rownames(segs)
+  inp$label <- factor(1:nrow(inp), labels=inp$vbl)
+  inp <- left_join(inp, segs, by=join_by(vbl))
+  if(any(inp$vbl == "zero"))inp <- inp[-which(inp$vbl == "zero"), ]
+  if(!make_plot){
+    res <- inp
+  }else{
+    g <- ggplot(inp) + 
+      geom_pointrange(aes(x=est, xmin = lwr, xmax=upr, y=label), size=.01) 
+    if("all" %in% ref_lines){
+      g <- g + geom_segment(aes(x=bound_start, xend=bound_end, y=stim_start, yend=stim_end), color="gray75", linetype=3)
+    }  
+    if( "ambiguous" %in% ref_lines){
+      g <- g + geom_segment(data = inp[which(inp$ambiguous), ], aes(x=bound_start, xend=bound_end, y=stim_start, yend=stim_end), color="gray75", linetype=3)
+    }
+    if(!any(c("ambiguous", "all", "none") %in% ref_lines)){
+      incl <- which(ref_lines %in% inp$vbl)
+      if(length(incl) == 0)stop("ref_lines should either be one of (all, ambiguous, or none) or a vector of names consistent with x$est$vbl.\n")
+      g <- g + geom_segment(data = inp[incl, ], aes(x=bound_start, xend=bound_end, y=stim_start, yend=stim_end), color="gray75", linetype=3)
+    }
+    res <- g
+  }
+  return(res)
+}
